eff041f1aed079a68ca1a861b299607507f203ba
ppphiiil <info@philmeinwelt.com>
2021-01-05 14:09:17 +0100

    till exercise 6

---
+++ index.js
@@ -0,0 +1,41 @@
+// ## These exercises are aimed at making you more familiar with string methods as well as string interpolation. Print your results of each exercise to the console. Use the terminal to view your results. 
+
+
+/* 1 */ console.log(`\n\n============== 1`);
+let string1 = "I can walk in the park all day!";
+console.log(string1.slice(18, 22));
+
+
+/* 2 */ console.log(`\n\n============== 2`);
+let string2 = "Hello World".toUpperCase();
+console.log(string2);
+
+
+/* 3 */ console.log(`\n\n============== 3`);
+let string3 = "Hello Earthing";
+console.log(string3.toLowerCase());
+
+
+/* 4 */ console.log(`\n\n============== 4`);
+
+let string4 = "JavaScript";
+console.log(string4.slice(3, 6));
+
+
+/* 5 */ console.log(`\n\n============== 5`);
+//Check if the sentence "nice shoes" contains the letter l or n. 
+
+let string5 = "nice shoes";
+console.log(string5);
+console.log(`n: ${string5.includes("n")}\nl: ${string5.includes("l")}`);
+
+
+/* 6 */  console.log(`\n\n============== 6`);
+
+/* 7 */
+
+/* 8 */
+
+/* 9 */
+
+/* 10 */
